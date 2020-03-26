078cfecad6dc85d8837b69ca6bc9cd6d730cd949
Leonardo Sbarai <leonardo.sbarai@gmail.com>
2020-03-26 18:21:05 -0300

    Alteração da classe dos motivos da exception para ENUM.

--- src/main/java/cadastroexception/MotivoCadastroInvalido.java
+++ src/main/java/cadastroexception/MotivoCadastroInvalido.java
@@ -1,29 +1,29 @@
 package cadastroexception;
 
-public class MotivoCadastroInvalido {
+public enum MotivoCadastroInvalido {
 
-    public static final MotivoCadastroInvalido CODIGO_PRODUTO_NULO = new MotivoCadastroInvalido(1, "Código do produto não pode ser nulo.");
-    public static final MotivoCadastroInvalido DESCRICAO_PRODUTO_NULA = new MotivoCadastroInvalido(2, "Descrição do produto não pode ser nula.");
-    public static final MotivoCadastroInvalido PRODUTO_SEM_PRECO = new MotivoCadastroInvalido(3, "Produto sem preço.");
-    public static final MotivoCadastroInvalido PRODUTO_EXISTENTE = new MotivoCadastroInvalido(4, "Produto já existe na lista.");
+    CODIGO_PRODUTO_NULO(1, "Código do produto não pode ser nulo."),
+    DESCRICAO_PRODUTO_NULA(2, "Descrição do produto não pode ser nula."),
+    PRODUTO_SEM_PRECO(3, "Produto sem preço."),
+    PRODUTO_EXISTENTE(4, "Produto já existe na lista."),
 
-    public static final MotivoCadastroInvalido USUARIO_NULO = new MotivoCadastroInvalido(5, "Nome do usuário não pode ser nulo.");
-    public static final MotivoCadastroInvalido EMAIL_USUARIO_NULO = new MotivoCadastroInvalido(6, "E-mail do usuário não pode ser nulo.");
-    public static final MotivoCadastroInvalido FORMATO_EMAIL_USUARIO_INCORRETO = new MotivoCadastroInvalido(7, "Formato de e-mail incorreto.");
-    public static final MotivoCadastroInvalido EMAIL_USUARIO_EXISTENTE = new MotivoCadastroInvalido(8, "E-mail já cadastrado para outro usuário.");
+    USUARIO_NULO(5, "Nome do usuário não pode ser nulo."),
+    EMAIL_USUARIO_NULO(6, "E-mail do usuário não pode ser nulo."),
+    FORMATO_EMAIL_USUARIO_INCORRETO(7, "Formato de e-mail incorreto."),
+    EMAIL_USUARIO_EXISTENTE(8, "E-mail já cadastrado para outro usuário."),
 
-    public static final MotivoCadastroInvalido QUANTIDADE_ITEMPEDIDO_NULA = new MotivoCadastroInvalido(9, "Quantidade não pode ser nula.");
-    public static final MotivoCadastroInvalido QUANTIDADE_ITEMPEDIDO_INCORRETA = new MotivoCadastroInvalido(10, "Quantidade incorreta.");
-    public static final MotivoCadastroInvalido PRODUTO_ITEMPEDIDO_NULO = new MotivoCadastroInvalido(11, "Produto não pode ser nulo.");
-    public static final MotivoCadastroInvalido REMOCAO_QUANTIDADE_INCORRETA_ITEMPEDIDO = new MotivoCadastroInvalido(12, "Quantidade não pode ser removida em sua totalidade.");
+    QUANTIDADE_ITEMPEDIDO_NULA(9, "Quantidade não pode ser nula."),
+    QUANTIDADE_ITEMPEDIDO_INCORRETA(10, "Quantidade incorreta."),
+    PRODUTO_ITEMPEDIDO_NULO(11, "Produto não pode ser nulo."),
+    REMOCAO_QUANTIDADE_INCORRETA_ITEMPEDIDO(12, "Quantidade não pode ser removida em sua totalidade."),
 
-    public static final MotivoCadastroInvalido USUARIO_NULO_PEDIDO = new MotivoCadastroInvalido(13, "Usuário não pode ser nulo.");
-    public static final MotivoCadastroInvalido PEDIDO_SEM_ITENS = new MotivoCadastroInvalido(14, "Pedido sem itens.");
+    USUARIO_NULO_PEDIDO(13, "Usuário não pode ser nulo."),
+    PEDIDO_SEM_ITENS(14, "Pedido sem itens.");
 
     private Integer codigo;
     private String mensagem;
 
-    public MotivoCadastroInvalido(Integer codigo, String mensagem) {
+    MotivoCadastroInvalido(Integer codigo, String mensagem) {
         this.codigo = codigo;
         this.mensagem = mensagem;
     }
