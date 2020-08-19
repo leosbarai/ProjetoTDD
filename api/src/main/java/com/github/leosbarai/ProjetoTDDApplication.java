631a874f392cb7631b951b7a6ab9b2a7eb39c8ea
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2020-08-19 09:57:54 -0300

    Cadastro do application.properties.

---
+++ api/src/main/java/com/github/leosbarai/ProjetoTDDApplication.java
@@ -0,0 +1,12 @@
+package com.github.leosbarai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTDDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoTDDApplication.class, args);
+    }
+}
--- core/src/main/java/entity/Usuario.java
+++ core/src/main/java/entity/Usuario.java
@@ -1,9 +1,17 @@
 package entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private String email;
 
